d8fab10bf71e64cbabbd4f41bba088e862a344cc
Nandan Kumar <maachandi2@gmail.com>
2025-12-13 15:39:21 +0530

    initialize project structure

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='bg-red-200 container m-auto text-center font-bold text-2xl'>Project</h1>
+    </>
+  )
+}
+
+export default App
